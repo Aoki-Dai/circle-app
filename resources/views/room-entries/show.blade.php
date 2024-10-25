82409a2d55e61e331daed7a7e309202bf80c6286
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-10-25 23:47:07 +0900

    入室記録のeditボタンdeleteボタンを追加する

--- resources/views/room-entries/show.blade.php
+++ resources/views/room-entries/show.blade.php
@@ -23,6 +23,12 @@
                 <div>
                     <strong>Exit Time:</strong> {{ $roomEntry->exit_time }}
                 </div>
+                <a href="{{ route('room-entries.edit', $roomEntry) }}" class="inline-block px-4 py-2 text-white transition duration-300 ease-in-out transform bg-yellow-500 rounded hover:bg-yellow-700 hover:scale-105">Edit</a>
+                <form action="{{ route('room-entries.destroy', $roomEntry) }}" method="POST" style="display:inline;" onsubmit="return confirm('この入室記録を削除してもよろしいですか？');">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="inline-block px-4 py-2 text-white transition duration-300 ease-in-out transform bg-red-500 rounded hover:bg-red-700 hover:scale-105">Delete</button>
+                </form><br>
                 <a href="{{ route('room-entries.index') }}" class="btn btn-primary">Back</a>
             </div>
         </div>
