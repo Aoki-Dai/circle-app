5e71cc237ce03ede019a267c10ae503784cbe761
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-12-28 04:57:00 +0900

    入力ノーマライズの無効化
    ミドルウェアからConvertEmptyStringsToNullとTrimStringsを削除

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
+use Illuminate\Foundation\Http\Middleware\TrimStrings;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->remove([
+            ConvertEmptyStringsToNull::class,
+            TrimStrings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
