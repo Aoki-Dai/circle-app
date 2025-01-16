5bd83c7d02e801af1f704621ce1bef27742f562f
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2025-01-16 23:36:19 +0900

    LPにもファビコンを設定する #31

--- resources/views/welcome-lp.blade.php
+++ resources/views/welcome-lp.blade.php
@@ -6,6 +6,8 @@
 
         <title>サークルなう</title>
 
+        <link rel="shortcut icon" href="{{ asset('/circle-now-logo-1.svg') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
